d178ce823798021e24e343b918c60bccf2ceb09a
Alejandro Cerezo <alce65@hotmail.es>
2026-07-15 12:45:17 +0200

    Add filters to Courses Page

---
+++ ng22/projects/demo-01/src/app/features/courses/components/filter/filter-options.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterOptions } from './filter-options';
+
+describe('FilterOptions', () => {
+  let component: FilterOptions;
+  let fixture: ComponentFixture<FilterOptions>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FilterOptions],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FilterOptions);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
