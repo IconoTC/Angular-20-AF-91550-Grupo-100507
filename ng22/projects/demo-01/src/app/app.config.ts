8ed72245fff11eef7f37dfce70bd8c02985cf47b
Alejandro Cerezo <alce65@hotmail.es>
2026-07-16 10:14:15 +0200

    Use build-in pipes and create custom pipes

--- ng22/projects/demo-01/src/app/app.config.ts
+++ ng22/projects/demo-01/src/app/app.config.ts
@@ -1,17 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import localeEs from '@angular/common/locales/es'
+import {registerLocaleData} from '@angular/common';
 import { routes } from './app.routes';
 import { ERROR_LEVEL } from './core/services/logger';
 import { environment } from '../environments/environment';
 
+registerLocaleData(localeEs);
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(), 
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     {
       provide: ERROR_LEVEL,
       useValue: environment.LOGGER_LEVEL,
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
     }
   ],
 };
--- ng22/projects/demo-01/src/app/features/home/components/info/info.ts
+++ ng22/projects/demo-01/src/app/features/home/components/info/info.ts
@@ -1,9 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { Time } from '../../../../core/services/time';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { TruncatePipe } from '../../../../core/pipes/truncate-pipe';
 
 @Component({
   selector: 'ind-info',
-  imports: [],
+  imports: [DatePipe, TitleCasePipe, TruncatePipe],
   template: ` <h3>Información del proyecto</h3>
     <p>Este proyecto es un ejemplo de uso de Angular 22 y sus nuevas características.</p>
     <ul>
@@ -11,42 +13,50 @@ import { Time } from '../../../../core/services/time';
       <li>TypeScript 6.0</li>
       <li>ES2026</li>
     </ul>
+    <h4>Uso del pipe Truncate</h4>
+    <p>[30]: {{ message() | truncate: 30 }}</p>
+    <p>[40]: {{ message() | truncate: 40 }}</p>
     <footer>
       <ul>
         <li>Autor: {{ author() }}</li>
-        <li>Fecha: {{ currentDate() }}</li>
+        <li>Fecha: {{ currentDate() | date: 'fullDate' | titlecase }}</li>
       </ul>
       <p>TimeStamp: {{ ts.getTime() }}</p>
     </footer>`,
   styles: `
-  ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
 
-p {
-  max-width: 23rem;
-}
+    p {
+      max-width: 23rem;
+    }
 
-footer {
-  font-size: 0.9em;
-  background-color: var(--color-background-primary);
-  color: var(--color-primary-hot);
-  border-top: 2px solid var(--color-primary);
-  border-radius: 0.5rem;
-  margin-top: 1rem;
-  
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}`,
+    footer {
+      font-size: 0.9em;
+      background-color: var(--color-background-primary);
+      color: var(--color-primary-hot);
+      border-top: 2px solid var(--color-primary);
+      border-radius: 0.5rem;
+      margin-top: 1rem;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  `,
 })
 export class Info {
   protected readonly author = signal('Alejandro Cerezo');
-  protected readonly currentDate = signal(new Date().toLocaleDateString());
+  protected readonly currentDate = signal(new Date());
 
-  protected readonly ts = inject(Time)
+  protected readonly ts = inject(Time);
+
+  protected readonly message = signal(
+    'Esto es un ejemplo de texto largo, para mostrar solo una parte de él',
+  );
 
   // Forma previa de importar los servicios
   // constructor(protected ts: Time ) {
